45497554bd90a9f6bae2887fa89469d7a7e032a1
Shaun Wilesmith <shaun.wilesmith@gmail.com>
2018-02-27 08:54:09 +1000

    Added real phone number to header and footer.

--- theme/elf/default/header.inc.php
+++ theme/elf/default/header.inc.php
@@ -13,7 +13,7 @@
 	<div class="grid-container top-contact-bar-container">
 		<div class="grid-x">
 			<div class="cell text-right contact-bar">
-				<span class="phone-contact"><span class="fa fa-phone"></span> 0400 123 456</span> or
+				<span class="phone-contact"><span class="fa fa-phone"></span> <a href="tel:0403575410">0403 575 410</a></span> or
 				<span class="email-contact"><a href="<?php get_site_url(); ?>/contact-us"><span class="fa fa-envelope"></span> Email</a></span>
 			</div>
 		</div>
